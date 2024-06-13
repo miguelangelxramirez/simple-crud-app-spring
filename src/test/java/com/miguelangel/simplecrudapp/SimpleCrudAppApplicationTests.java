26ca42ddbfdffc7b10231501d0d7fd4f5ec7c048
Miguel Angel <miguelangelxramirez@gmail.com>
2024-06-13 20:10:12 +0200

    First commit

---
+++ src/test/java/com/miguelangel/simplecrudapp/SimpleCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelangel.simplecrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
