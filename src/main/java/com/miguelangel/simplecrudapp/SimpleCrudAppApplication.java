3c197f9428a82579b26702443555fa356df235a7
Miguel Angel <miguelangelxramirez@gmail.com>
2024-06-13 20:12:53 +0200

    Second commit to test

--- src/main/java/com/miguelangel/simplecrudapp/SimpleCrudAppApplication.java
+++ src/main/java/com/miguelangel/simplecrudapp/SimpleCrudAppApplication.java
@@ -3,6 +3,7 @@ package com.miguelangel.simplecrudapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SimpleCrudAppApplication {
 
